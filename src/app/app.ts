b813083de09edbc8203aa1ed241523043a811902
Parvez Raza Siddiqui <parvez.siddiqui@biz2x.com>
2025-09-23 17:36:59 +0530

    added contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Hero } from './sections/hero/hero';
 import { SocialIcons } from './components/social-icons/social-icons';
 import { Email } from './components/email/email';
 import { About } from './sections/about/about';
+import { Contact } from './sections/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Hero,SocialIcons,Email,About],
+  imports: [RouterOutlet,Navbar,Hero,SocialIcons,Email,About,Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sections/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Button } from '../../components/button/button';
+
+@Component({
+  selector: 'app-contact',
+  imports: [Button],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css'
+})
+export class Contact {
+
+}
